9d2babc0db841988ff586cc11fedf9fbc338858a
Mark <gllen@gllen.com>
2014-12-04 21:24:53 -0600

    zxdsfcgrwshwrb

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var spp = require('./lib/spp.js');
-var _ = require('underscore');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-
-var usernames = {};
-var rooms = {};
-
+var port = 80;
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+
+var spp = require('./lib/spp.js');
+var _ = require('underscore');
+var usernames = {};
 
 app.use(express.static(__dirname + '/public'));
 
